645a81fcd9e12cc8021340c9e01594e60bd0ffa5
Leccon1 <dlavania703@gamil.com>
2025-12-03 00:05:59 +0300

    feat: add start data cities

---
+++ src/components/layuot/Main/components/CitiesSection/data/cities.data.js
@@ -0,0 +1,19 @@
+const citiesData = [
+    {
+        id: 1,
+        city: 'Tokyo',
+        image: 'tokyo.jpg',
+    },
+    {
+        id: 2,
+        city: 'Osaka',
+        image: 'osaka.jpg',
+    },
+    {
+        id: 3,
+        city: 'Kyoto',
+        image: 'kyoto.jpg',
+    },
+]
+
+export default citiesData
