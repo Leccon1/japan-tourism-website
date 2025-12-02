31324e2fab95b276fbd8a53e5c0f5f464cd7e439
Leccon1 <dlavania703@gamil.com>
2025-12-03 00:33:24 +0300

    feat: add citiesSection component for Main component

--- src/components/layuot/Main/components/CitiesSection/CitiesSection.jsx
+++ src/components/layuot/Main/components/CitiesSection/CitiesSection.jsx
@@ -1,7 +1,19 @@
-import styles from '../main.module.scss'
-
-const citiesLine = () => {
-  return <section className={styles.citiesLine}></section>
+import styles from './CitiesSection.module.scss'
+import citiesData from './data/cities.data'
+const CitiesLine = () => {
+  return (
+    <section className={styles.citiesLine}>
+      {citiesData.map((item) => (
+        <div className={styles.citiesLineItem} key={item.city}>
+          <img className={styles.citiesLineItemImage} src={item.image} alt={item.city} />
+          <div className={styles.citiesLineItemContent}>
+            <p className={styles.citiesLineItemDescription}>{item.description}</p>
+            <p className={styles.citiesLineItemCity}>{item.city}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  )
 }
 
-export default citiesLine
+export default CitiesLine
