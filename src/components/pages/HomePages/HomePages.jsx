089ed047b5567b65b6e370f547c3c0b2f26f9382
Leccon1 <dlavania703@gamil.com>
2025-12-03 00:31:47 +0300

    feat: connect main component

---
+++ src/components/pages/HomePages/HomePages.jsx
@@ -0,0 +1,15 @@
+import Container from '../../layuot/Container/Container'
+import Header from '../../layuot/Header/Header'
+import Main from '../../layuot/Main/Main'
+const HomePages = () => {
+  return (
+    <div>
+      <Container type={'full'}>
+        <Header />
+        <Main />
+      </Container>
+    </div>
+  )
+}
+
+export default HomePages
