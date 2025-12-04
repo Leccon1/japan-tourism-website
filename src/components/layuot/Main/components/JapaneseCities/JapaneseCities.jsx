92d7ee7ebfc6deb882d491ca6612c69fdfe457cd
Leccon1 <dlavania703@gamil.com>
2025-12-04 21:11:35 +0300

    chore: add empty JapaneseCities component and module styles

---
+++ src/components/layuot/Main/components/JapaneseCities/JapaneseCities.jsx
@@ -0,0 +1,7 @@
+import styles from './japaneseCities.module.scss'
+
+const JapaneseCities = () => {
+  return <section className={styles.JapaneseCities}></section>
+}
+
+export default JapaneseCities
