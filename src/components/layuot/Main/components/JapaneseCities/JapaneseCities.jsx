1e7ccdb189fb52d53e83585fb5866d8e5cf31a47
Leccon1 <dlavania703@gamil.com>
2025-12-04 22:12:19 +0300

    feat: add line button change content

--- src/components/layuot/Main/components/JapaneseCities/JapaneseCities.jsx
+++ src/components/layuot/Main/components/JapaneseCities/JapaneseCities.jsx
@@ -1,7 +1,26 @@
+import citiesContentData from './data/citiesContent.data'
 import styles from './japaneseCities.module.scss'
-
 const JapaneseCities = () => {
-  return <section className={styles.JapaneseCities}></section>
+  return (
+    <section className={styles.japaneseCities}>
+      <div className={styles.japaneseCitiesItem}>
+        <img className={styles.japaneseCitiesItemImage} src="" alt="" />
+        <div className={styles.japaneseCitiesItemInfo}>
+          <h2 className={styles.japaneseCitiesItemTitle}></h2>
+          <p className={styles.japaneseCitiesItemDescription}></p>
+          <ul className={styles.japaneseCitiesItemDescriptionButtonList}>
+            {citiesContentData.map((city) => (
+              <li key={city.name} className={styles.japaneseCitiesItemDescriptionButtonListItem}>
+                <button key={city.name} type="button" data-city={city.name}>
+                  {city.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default JapaneseCities
