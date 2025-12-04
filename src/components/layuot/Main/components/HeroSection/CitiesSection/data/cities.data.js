fb6c46b1aecf14a9be558e3f3fbef24c76bc047f
Leccon1 <dlavania703@gamil.com>
2025-12-04 21:27:48 +0300

    fix: update path to icons cities

--- src/components/layuot/Main/components/HeroSection/CitiesSection/data/cities.data.js
+++ src/components/layuot/Main/components/HeroSection/CitiesSection/data/cities.data.js
@@ -2,22 +2,22 @@ const citiesData = [
   {
     city: 'Tokyo',
     description: 'Get intothe future in',
-    image: new URL('@images/tokyo.svg', import.meta.url).href,
+    image: new URL('@icons/tokyo.svg', import.meta.url).href,
   },
   {
     city: 'Osaka',
     description: 'Turn back the time',
-    image: new URL('@images/osaka.svg', import.meta.url).href,
+    image: new URL('@icons/osaka.svg', import.meta.url).href,
   },
   {
     city: 'Kyoto',
     description: 'Get impressed by',
-    image: new URL('@images/kyoto.svg', import.meta.url).href,
+    image: new URL('@icons/kyoto.svg', import.meta.url).href,
   },
   {
     city: 'Nara',
     description: 'Get intothe future ',
-    image: new URL('@images/nara.svg', import.meta.url).href,
+    image: new URL('@icons/nara.svg', import.meta.url).href,
   },
 ]
 
