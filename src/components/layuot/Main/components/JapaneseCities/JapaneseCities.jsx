1f5da543204fb2e5f549dc7ed2ce8b93079ce91d
Leccon1 <dlavania703@gamil.com>
2025-12-04 22:49:01 +0300

    feat: add button list to switch content from citiesContent.data.js

--- src/components/layuot/Main/components/JapaneseCities/JapaneseCities.jsx
+++ src/components/layuot/Main/components/JapaneseCities/JapaneseCities.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react'
+
 import citiesContentData from './data/citiesContent.data'
 import styles from './japaneseCities.module.scss'
 const JapaneseCities = () => {
+  const [selectedCity, setSelectedCity] = useState(citiesContentData[0])
+  const handleClick = (e) => {
+    const cityName = e.currentTarget.dataset.city
+    const cityObj = citiesContentData.find((city) => city.name === cityName)
+    if (cityObj) {
+      setSelectedCity(cityObj)
+    }
+  }
+
   return (
     <section className={styles.japaneseCities}>
       <div className={styles.japaneseCitiesItem}>
-        <img className={styles.japaneseCitiesItemImage} src="" alt="" />
+        <img className={styles.japaneseCitiesItemImage} src={selectedCity.image} alt="" />
         <div className={styles.japaneseCitiesItemInfo}>
-          <h2 className={styles.japaneseCitiesItemTitle}></h2>
-          <p className={styles.japaneseCitiesItemDescription}></p>
+          <h2 className={styles.japaneseCitiesItemTitle}>{selectedCity.name}</h2>
+          <p className={styles.japaneseCitiesItemDescription}>{selectedCity.description}</p>
           <ul className={styles.japaneseCitiesItemDescriptionButtonList}>
             {citiesContentData.map((city) => (
               <li key={city.name} className={styles.japaneseCitiesItemDescriptionButtonListItem}>
-                <button key={city.name} type="button" data-city={city.name}>
+                <button key={city.name} type="button" onClick={handleClick} data-city={city.name}>
                   {city.name}
                 </button>
               </li>
